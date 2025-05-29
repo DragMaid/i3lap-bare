3c294937909785eba8d46ef6302d6d2dfdb37d41
VuHoangHaiBinh <vhoanghaibinh@gmail.com>
2025-05-29 13:26:35 +0800

    update: set undotree dir

--- .config/nvim/lua/configs/undotree-config.lua
+++ .config/nvim/lua/configs/undotree-config.lua
@@ -2,3 +2,4 @@ vim.g.undotree_WindowLayout = 1
 vim.g.undotree_CustomUndotreeCmd  = 'topleft vertical 30 new'
 vim.g.undotree_CustomDiffpanelCmd = 'belowright 10 new'
 vim.g.undotree_SetFocusWhenToggle = 1
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
